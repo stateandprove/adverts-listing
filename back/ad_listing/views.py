4abdedafd8821715f16e38241a8e2cae5d723eb9
stateandprove <sphere050@yandex.ru>
2024-08-05 00:03:18 +0200

    back: removed unused import

--- back/ad_listing/views.py
+++ back/ad_listing/views.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
 from django.http import Http404
